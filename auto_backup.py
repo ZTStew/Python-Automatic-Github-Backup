b5cc216d385d279e63b4af751fa043891d8d032b
ZTStew <zt.stewart.5@gmail.com>
2025-08-08 23:55:56 -0400

    Check for provided paths to be Repositories working

--- auto_backup.py
+++ auto_backup.py
@@ -4,8 +4,8 @@ Description:
   Program is intended to be used with Windows Task Scheduler to automatically commit changes to Github
 
 Requirements:
-  [] Read .txt file to get file path(s) for local repo location(s)
-  [] File path location must already be a Github repo
+  [x] Read .txt file to get file path(s) for local repo location(s)
+  [x] File path location must already be a Github repo
   [] Navigate to local repo(s)
   [] Run: git add . | git commit -a --allow-empty-message -m "" | git push
 
@@ -14,12 +14,45 @@ Resources:
 """
 
 import os
+import logging as log
 
-with open("./paths/paths.txt") as file:
+log_path = os.path.dirname(os.path.abspath(__file__)) + '\\log\\run.log'
+
+log.basicConfig(
+    filename= log_path,
+    level=log.DEBUG,
+    format='%(asctime)s - %(levelname)s - %(message)s'
+)
+# log.debug("debug message")
+# log.info("info message")
+# log.warning("warning message")
+# log.error("error message")
+# log.critical("critical message")
+
+#############################################################################################################
+#############################################################################################################
+#############################################################################################################
+
+
+log.critical("### ### ### V Program Starts V ### ### ###")
+
+path = "./paths/paths.txt"
+
+with open(path) as file:
   for line in file:
-    line = line.strip("\n")
-    print(line)
+
+    # non-existant lines are being read. Not a major issue due to confirmation code.
+    line = line.strip("\n").strip("\r")
+
+    if os.path.exists(line):
+      if os.path.exists(line + "/.git"):
+        print("valid path")
+        print(line + "/.git")
+        log.info("Valid Directory, Valid Repository Found At: " + line)
+      else:
+        log.info("Valid Directory, No Repository Found At: " + line)
+    else:
+      log.info("Invalid Directory: " + line)
 
 file.close()
-
-# path = os.path.dirname(os.path.abspath(__file__)) + '\\Log\\template.log'
+log.critical("Program Terminated")
